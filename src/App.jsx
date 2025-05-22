d2ce8f0d974c80e6a248b3015b22d66957a1e1d3
Saksham Mittal <saksham.mittal@nexla.com>
2025-05-22 21:55:57 +0530

    Replace deprecated react-image-lightbox with yet-another-react-lightbox; update package-lock.json and package.json accordingly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
-import "react-image-lightbox/style.css";
 import './App.css';
 
 import HomeOne from './pages/HomeOne';
